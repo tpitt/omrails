cfe40a6cc3973d8e33fe08088a7a28221300f5a8
Tyson Pitt <tysonpitt@gmail.com>
2014-01-17 16:51:05 -0500

    Add Bootstrap

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Omrails::Application.routes.draw do
+  
+  get 'about' => 'pages#about'
+
+  root :to => 'Pages#home'
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
